9a1b9306b4c4a00964d4ccf6ff77859fbb13333e
Ammar <ammaralaa470@gmail.com>
2025-11-17 22:28:56 +0200

    set up

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="h1-bold font-space-grotesk">Welcome in space grotesk</h1>
+      <h1 className="h1-bold ">Welcome in Inter</h1>      
+    </div> 
+  );
+}
